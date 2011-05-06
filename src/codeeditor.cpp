3fcaa90f47ca33e883ccb38faf3ff44617f109f8
Giles Bathgate <gilesbathgate@gmail.com>
2011-05-06 21:39:59 +0100

    Highlight the current line number (i.e. highlight the number not the line)

--- src/codeeditor.cpp
+++ src/codeeditor.cpp
@@ -76,13 +76,17 @@ void CodeEditor::lineNumberAreaPaintEvent(QPaintEvent* event)
 
 	QTextBlock block = firstVisibleBlock();
 	int blockNumber = block.blockNumber();
+	int currentBlock = textCursor().block().blockNumber();
 	int top = (int) blockBoundingGeometry(block).translated(contentOffset()).top();
 	int bottom = top + (int) blockBoundingRect(block).height();
 
 	while(block.isValid() && top <= event->rect().bottom()) {
 		if(block.isVisible() && bottom >= event->rect().top()) {
 			QString number = QString::number(blockNumber + 1);
-			painter.setPen(Qt::darkGray);
+			if(blockNumber==currentBlock)
+				painter.setPen(Qt::black);
+			else
+				painter.setPen(Qt::darkGray);
 			painter.drawText(0, top, lineNumberArea->width(), fontMetrics().height(),
 							 Qt::AlignRight, number);
 		}
