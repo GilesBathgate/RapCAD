GilesBathgate/RapCAD

commit 02f190f78ca30ff3404b3fb1db84a46ae436199b

Implemented proper handling of const an param variables.

src/evaluator.cpp
@@ -322,16 +322,16 @@ void Evaluator::visit(AssignStatement* stmt)
 	stmt->getVariable()->accept(*this);
 	QString name = context->currentName;
 
+	Value* lvalue = context->currentValue;
+
 	Value* result;
 	switch(stmt->getOperation()) {
 	case Expression::Increment: {
-		Value* v = context->lookupVariable(name);
-		result=Value::operation(v,Expression::Increment);
+		result=Value::operation(lvalue,Expression::Increment);
 		break;
 	}
 	case Expression::Decrement: {
-		Value* v = context->lookupVariable(name);
-		result=Value::operation(v,Expression::Decrement);
+		result=Value::operation(lvalue,Expression::Decrement);
 		break;
 	}
 	default: {
@@ -344,7 +344,20 @@ void Evaluator::visit(AssignStatement* stmt)
 	}
 
 	result->setName(name);
-	context->addVariable(result);
+	result->setType(lvalue->getType());
+	switch(lvalue->getType()){
+	case Variable::Const:
+		if(!context->addVariable(result))
+			output << "Warning: Attempt to alter const variable '" << name << "'\n";
+		break;
+	case Variable::Param:
+		if(!context->addVariable(result))
+			output << "Warning: Attempt to alter param variable '" << name << "'\n";
+		break;
+	default:
+		context->setVariable(result);
+		break;
+	}
 }
 
 void Evaluator::visit(VectorExpression* exp)
@@ -446,7 +459,8 @@ void Evaluator::visit(Literal* lit)
 void Evaluator::visit(Variable* var)
 {
 	QString name = var->getName();
-	Value* v=context->lookupVariable(name);
+	Variable::Type_e type=var->getType();
+	Value* v=context->lookupVariable(name,type);
 
 	context->currentValue=v;
 	context->currentName=name;

src/context.cpp
@@ -64,19 +64,32 @@ Function* Context::lookupFunction(QString name)
 	return functions.value(name);
 }
 
-void Context::addVariable(Value* v)
+bool Context::addVariable(Value* v)
+{
+	QString name=v->getName();
+	if(!variables.contains(name)) {
+		variables.insert(name,v);
+		return true;
+	}
+	return false;
+}
+
+void Context::setVariable(Value* v)
 {
 	variables.insert(v->getName(),v);
 }
 
-Value* Context::lookupVariable(QString name)
+Value* Context::lookupVariable(QString name,Variable::Type_e type)
 {
-	if(variables.contains(name))
+	if(variables.contains(name)) {
 		return variables.value(name);
-	else if(parent)
-		return parent->lookupVariable(name);
-	else
-		return new Value(); //undef
+	} else if(parent) {
+		return parent->lookupVariable(name,type);
+	} else {
+		Value* v=new Value(); //undef
+		v->setType(type);
+		return v;
+	}
 }
 
 void Context::addModule(Module* mod)

src/context.h
@@ -43,8 +43,9 @@ public:
 	QList<Node*> currentNodes;
 	QList<Node*> inputNodes;
 
-	Value* lookupVariable(QString);
-	void addVariable(Value*);
+	Value* lookupVariable(QString,Variable::Type_e=Variable::Var);
+	bool addVariable(Value*);
+	void setVariable(Value*);
 
 	Module* lookupModule(QString);
 	void addModule(Module* mod);

src/value.cpp
@@ -22,6 +22,17 @@
 
 Value::Value()
 {
+	type=Variable::Const;
+}
+
+void Value::setType(Variable::Type_e t)
+{
+	type=t;
+}
+
+Variable::Type_e Value::getType() const
+{
+	return type;
 }
 
 void Value::setName(QString name)

src/value.h
@@ -22,11 +22,14 @@
 #include <QString>
 #include "iterator.h"
 #include "expression.h"
+#include "variable.h"
 
 class Value
 {
 public:
 	Value();
+	void setType(Variable::Type_e);
+	Variable::Type_e getType() const;
 	void setName(QString);
 	QString getName() const;
 	virtual QString getValueString() const;
@@ -64,6 +67,7 @@ protected:
 	template <class A, class B>
 	A basicOperation(B,Expression::Operator_e);
 private:
+	Variable::Type_e type;
 	QString name;
 	virtual Value* operation(Expression::Operator_e);
 	virtual Value* operation(Value&,Expression::Operator_e);

commit 252572ac62993c7862a0749c2e17502b18f1cd00

Fix breakage of For Statement.
broken by commit 02f190f78ca30ff3404b3fb1db84a46ae436199b

src/evaluator.cpp
@@ -257,7 +257,7 @@ void Evaluator::visit(ForStatement* forstmt)
 
 		Value* v = i->currentItem();
 		v->setName(first->getName());
-		context->addVariable(v);
+		context->setVariable(v);
 
 		forstmt->getStatement()->accept(*this);
 