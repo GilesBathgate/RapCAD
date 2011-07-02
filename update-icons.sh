23ec74dd253cb4bd1005f4bca4bc644eb01dd07c
Giles Bathgate <gilesbathgate@gmail.com>
2011-07-03 00:40:20 +0100

    Include smaller icons in theme resource too for menu items.

--- update-icons.sh
+++ update-icons.sh
@@ -7,7 +7,9 @@ echo '    <file alias="index.theme">icons/gnome/index.theme</file>' >> icons.qrc
 sed -n 's/.*fromTheme("\(.*\)"));.*/\1.png/p' src/mainwindow.cpp | while read img
 do
 cp /usr/share/icons/gnome/22x22/*/$img icons/gnome/22x22/
+cp /usr/share/icons/gnome/16x16/*/$img icons/gnome/16x16/
 echo "    <file alias=\"22x22/$img\">icons/gnome/22x22/$img</file>" >> icons.qrc
+echo "    <file alias=\"16x16/$img\">icons/gnome/16x16/$img</file>" >> icons.qrc
 done
 echo '</qresource>' >> icons.qrc
 echo '</RCC>' >> icons.qrc
