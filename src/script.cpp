GilesBathgate/RapCAD

commit b945a43b3f7b2c0520150f2b732328f54daf21d0

While its fresh in my mind also added ability for use declaration to have an optional as clause. This allows imported modules to have a namespace, and will also enable 'use <file.dxf> as mymodule' syntax which is much better for dependency tracking.

src/script.cpp
@@ -37,3 +37,8 @@ QVector<Declaration*> Script::getDeclarations()
 {
     return this->declarations;
 }
+
+void Script::addDeclaration(Declaration* dec)
+{
+    declarations.append(dec);
+}

src/abstractsyntaxtreebuilder.h
@@ -51,8 +51,10 @@ class AbstractSyntaxTreeBuilder
 {
 public:
     virtual ~AbstractSyntaxTreeBuilder(){}
-    virtual void BuildScript(QString*)=0;
+    virtual void BuildScript(Declaration*)=0;
     virtual void BuildScript(QVector<Declaration*>*)=0;
+    virtual Declaration* BuildUse(QString*)=0;
+    virtual Declaration* BuildUse(QString*,QString*)=0;
     virtual QVector<Declaration*>* BuildDeclarations()=0;
     virtual QVector<Declaration*>* BuildDeclarations(Declaration*)=0;
     virtual QVector<Declaration*>* BuildDeclarations(QVector<Declaration*>*)=0;

src/abstracttokenbuilder.h
@@ -42,6 +42,7 @@ public:
     virtual unsigned int BuildConst()=0;
     virtual unsigned int BuildParam()=0;
     virtual unsigned int BuildIf()=0;
+    virtual unsigned int BuildAs()=0;
     virtual unsigned int BuildElse()=0;
     virtual unsigned int BuildFor()=0;
     virtual unsigned int BuildReturn()=0;

src/dependencybuilder.cpp
@@ -26,12 +26,22 @@ DependencyBuilder::~DependencyBuilder()
 {
 }
 
-void DependencyBuilder::BuildScript(QString*)
+void DependencyBuilder::BuildScript(Declaration*)
 {
 }
 
 void DependencyBuilder::BuildScript(QVector<Declaration*>*)
 {
+}
+
+Declaration* DependencyBuilder::BuildUse(QString*)
+{
+    return NULL;
+}
+
+Declaration* DependencyBuilder::BuildUse(QString*,QString*)
+{
+    return NULL;
 }
 
 QVector<Declaration*>* DependencyBuilder::BuildDeclarations()

src/dependencybuilder.h
@@ -26,8 +26,10 @@ class DependencyBuilder : public AbstractSyntaxTreeBuilder
 public:
     DependencyBuilder();
     ~DependencyBuilder();
-    void BuildScript(QString*);
+    void BuildScript(Declaration*);
     void BuildScript(QVector<Declaration*>*);
+    Declaration* BuildUse(QString*);
+    Declaration* BuildUse(QString*,QString*);
     QVector<Declaration*>* BuildDeclarations();
     QVector<Declaration*>* BuildDeclarations(Declaration*);
     QVector<Declaration*>* BuildDeclarations(QVector<Declaration*>*);

src/lexer.l
@@ -55,6 +55,7 @@ WS [ \t\r\n]
 "const"		{ return tbuilder->BuildConst(); }
 "param"		{ return tbuilder->BuildParam(); }
 "if"			{ return tbuilder->BuildIf(); }
+"as"			{ return tbuilder->BuildAs(); }
 "else"			{ return tbuilder->BuildElse(); }
 "for"			{ return tbuilder->BuildFor(); }
 "return"		{ return tbuilder->BuildReturn(); }

src/moduleimport.cpp
@@ -36,6 +36,16 @@ QString ModuleImport::getImport()
     return this->import;
 }
 
+void ModuleImport::setNamespace(QString name)
+{
+    this->name_space = name;
+}
+
+QString ModuleImport::getNamespace()
+{
+    return this->name_space;
+}
+
 void ModuleImport::accept(Visitor& v)
 {
     v.visit(this);

src/moduleimport.h
@@ -29,9 +29,12 @@ public:
     ~ModuleImport();
     void setImport(QString);
     QString getImport();
+    void setNamespace(QString);
+    QString getNamespace();
     void accept(Visitor&);
 private:
     QString import;
+    QString name_space;
 };
 
 #endif // MODULEIMPORT_H

commit 86a7cccfae36665f6464d331df481e41f1fc2472

Fixed error introduced in commit b945a43b3f7b2c0520150f2b732328f54daf21d0
Previously prepend was used but this was changed to append. prepend is the
correct thing to do.

src/script.cpp
@@ -40,7 +40,7 @@ QList<Declaration*> Script::getDeclarations() const
 
 void Script::addDeclaration(Declaration* dec)
 {
-	declarations.append(dec);
+	declarations.prepend(dec);
 }
 
 void Script::removeDeclaration(Declaration* dec)