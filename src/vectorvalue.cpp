d15388d0a43a753579f2bcfe51925d8b94f27044
Giles Bathgate <gilesbathgate@gmail.com>
2011-08-24 19:32:46 +0100

    Renamed variable to more suitable name.

--- src/vectorvalue.cpp
+++ src/vectorvalue.cpp
@@ -94,22 +94,22 @@ Value* VectorValue::operation(Expression::Operator_e e)
 Value* VectorValue::operation(Value& v, Expression::Operator_e e)
 {
 	QList<Value*> result;
-	VectorValue* that = dynamic_cast<VectorValue*>(&v);
-	if(that) {
+	VectorValue* vec=dynamic_cast<VectorValue*>(&v);
+	if(vec) {
 		if(e==Expression::OuterProduct) {
-			for(int i=0; i<that->children.size(); i++)
-				result.append(Value::operation(this,e,that->children.at(i)));
+			for(int i=0; i<vec->children.size(); i++)
+				result.append(Value::operation(this,e,vec->children.at(i)));
 		} else if(e==Expression::Multiply || e==Expression::Divide) {
 			//TODO implement multiply and divide
 			return this;
 		} else if(e==Expression::Concatenate) {
 			result=this->children;
-			foreach(Value* child,that->children)
+			foreach(Value* child,vec->children)
 				result.append(child);
 		} else {
 			e=convertOperation(e);
-			for(int i=0; i<children.size() && i<that->children.size(); i++)
-				result.append(Value::operation(this->children.at(i),e,that->children.at(i)));
+			for(int i=0; i<children.size() && i<vec->children.size(); i++)
+				result.append(Value::operation(this->children.at(i),e,vec->children.at(i)));
 		}
 	}
 
