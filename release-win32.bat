773a9c10d896d14544b25c700eac3ae79f7fc877
Giles Bathgate <gilesbathgate@gmail.com>
2012-03-22 22:01:36 +0000

    Added git tasks to release-win32.bat

--- release-win32.bat
+++ release-win32.bat
@@ -1,7 +1,13 @@
 @echo off
 
+call C:\Qt\4.8.0\bin\qtvars.bat
+set PATH=%PATH%;%ProgramFiles%\Git\bin
 set PATH=%PATH%;%ProgramFiles%\NSIS
 
+pushd c:\rapcad && ^
+git pull && ^
+git reset --hard master && ^
+git clean -df && ^
 qmake CONFIG+=official && ^
 make -f Makefile.Release && ^
 make clean && ^
@@ -9,4 +15,5 @@ copy installer.nsi release && ^
 copy ..\rapcad-dlls\* release && ^
 pushd release && ^
 makensis.exe installer.nsi && ^
+popd
 popd
