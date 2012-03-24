33f6aecbf51db2521d40b476c2fd1bc4f89fb94c
Giles Bathgate <gilesbathgate@gmail.com>
2012-03-24 10:59:49 +0000

    Added automatic building of zip files to release script.

--- installer.nsi
+++ installer.nsi
@@ -4,18 +4,18 @@ InstallDir $PROGRAMFILES\RapCAD
 DirText "This will install RapCAD on your computer. Choose a directory"
 Section "install"
 SetOutPath $INSTDIR
-File rapcad.exe
-File libboost_thread-mgw46-mt-1_49.dll
-File libgcc_s_dw2-1.dll
-File libstdc++-6.dll
-File mingwm10.dll
-File QtCore4.dll
-File QtGui4.dll
-File QtOpenGL4.dll
-File libCGAL.dll
-File libCGAL_Core.dll
-File libmpfr-4.dll
-File libgmp-10.dll
+File release\rapcad.exe
+File release\libboost_thread-mgw46-mt-1_49.dll
+File release\libgcc_s_dw2-1.dll
+File release\libstdc++-6.dll
+File release\mingwm10.dll
+File release\QtCore4.dll
+File release\QtGui4.dll
+File release\QtOpenGL4.dll
+File release\libCGAL.dll
+File release\libCGAL_Core.dll
+File release\libmpfr-4.dll
+File release\libgmp-10.dll
 CreateShortCut $SMPROGRAMS\RapCAD.lnk $INSTDIR\rapcad.exe
 WriteUninstaller $INSTDIR\Uninstall.exe
 WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\RapCAD" "DisplayName" "RapCAD (remove only)"
--- release-win32.bat
+++ release-win32.bat
@@ -1,27 +1,41 @@
 @echo off
 
-call C:\Qt\4.8.0\bin\qtvars.bat
+call c:\Qt\4.8.0\bin\qtvars.bat
+set PATH=%PATH%;c:\MinGW\msys\1.0\bin
 set PATH=%PATH%;%ProgramFiles%\Git\bin
 set PATH=%PATH%;%ProgramFiles%\NSIS
+set hostdir=h:\rapcad
+set build=%hostdir%\build.log
+set error=%hostdir%\error.log
 
 pushd c:\rapcad && ^
 set /p version= < VERSION && ^
-echo Building RapCAD version %version% && ^
-git pull && ^
-git reset --hard master && ^
-git clean -df && ^
+echo Building RapCAD v%version% ^
+  > %build% && ^
+git pull ^
+  >> %build% 2>> %error% && ^
+git reset --hard master ^
+  >> %build% 2>> %error% && ^
+git clean -df ^
+  >> %build% 2>> %error% && ^
 qmake CONFIG+=official ^
-  >> build.log 2>> errors.log && ^
+  >> %build% 2>> %error% && ^
 make -f Makefile.Release ^
-  >> build.log 2>> errors.log && ^
+  >> %build% 2>> %error% && ^
 make clean ^
-  >> build.log 2>> errors.log && ^
-copy installer.nsi release ^
-  >> build.log 2>> errors.log && ^
+  >> %build% 2>> %error% && ^
 copy ..\rapcad-dlls\* release ^
-  >> build.log 2>> errors.log && ^
-pushd release && ^
+  >> %build% 2>> %error% && ^
 makensis.exe installer.nsi ^
-  >> build.log 2>> errors.log ^
-popd
+  >> %build% 2>> %error% && ^
+move rapcad_setup.exe rapcad_%version%_setup.exe ^
+  >> %build% 2>> %error% && ^
+move release rapcad-%version% ^
+  >> %build% 2>> %error% && ^
+zip rapcad_%version%.zip rapcad-%version% ^
+  >> %build% 2>> %error% && ^
+move rapcad_%version%_setup.exe %hostdir% ^
+  >> %build% 2>> %error% && ^
+move rapcad_%version%.zip %hostdir% ^
+  >> %build% 2>> %error% && ^
 popd
