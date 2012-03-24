cca92565c9589769d05552fcb77301de9c772a95
Giles Bathgate <gilesbathgate@gmail.com>
2012-03-24 07:18:32 +0000

    Add logging to release batch file.

--- release-win32.bat
+++ release-win32.bat
@@ -5,15 +5,23 @@ set PATH=%PATH%;%ProgramFiles%\Git\bin
 set PATH=%PATH%;%ProgramFiles%\NSIS
 
 pushd c:\rapcad && ^
+set /p version= < VERSION && ^
+echo Building RapCAD version %version% && ^
 git pull && ^
 git reset --hard master && ^
 git clean -df && ^
-qmake CONFIG+=official && ^
-make -f Makefile.Release && ^
-make clean && ^
-copy installer.nsi release && ^
-copy ..\rapcad-dlls\* release && ^
+qmake CONFIG+=official ^
+  >> build.log 2>> errors.log && ^
+make -f Makefile.Release ^
+  >> build.log 2>> errors.log && ^
+make clean ^
+  >> build.log 2>> errors.log && ^
+copy installer.nsi release ^
+  >> build.log 2>> errors.log && ^
+copy ..\rapcad-dlls\* release ^
+  >> build.log 2>> errors.log && ^
 pushd release && ^
-makensis.exe installer.nsi && ^
+makensis.exe installer.nsi ^
+  >> build.log 2>> errors.log ^
 popd
 popd
