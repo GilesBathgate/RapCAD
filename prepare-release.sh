10830b7aa336d820698c92e7e67b2bd9c2056a3e
Giles Bathgate <gilesbathgate@gmail.com>
2012-04-27 17:59:15 +0100

    Added little script to help prepare releases.

---
+++ prepare-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+newversion=$1
+if [ -z "$newversion" ]; then
+    echo "New version not specified"
+    exit
+fi
+
+currentversion=$(cat VERSION)
+
+export DEBEMAIL=gilesbathgate@gmail.com
+dch -v$newversion "New upstream version."
+
+sed "s/^Version:.*$/Version:        $newversion/" -i suse/rapcad.spec
+
+git log --oneline v$currentversion..HEAD > shortlog
+echo $newversion | cat - shortlog doc/release_notes.asciidoc > result
+mv result doc/release_notes.asciidoc
+rm shortlog
+vim doc/release_notes.asciidoc
+
+git add debian/changelog suse/rapcad.spec doc/release_notes.asciidoc
+git commit -m "Updated Release notes for upcoming version."
+
+echo $1 > VERSION
+git add VERSION
+git commit -m "RapCAD $newversion"
+
+git tag -s v$1 HEAD -m "RapCAD $newversion"
+
+echo "Now just 'git push --tags'"
