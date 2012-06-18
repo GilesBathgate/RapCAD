7867cdff45a69d0bbbab97204fa5c6248229abc3
Giles Bathgate <gilesbathgate@gmail.com>
2012-06-18 19:12:31 +0100

    Implemented module stub for rotateextrude.

--- src/module/rotateextrudemodule.cpp
+++ src/module/rotateextrudemodule.cpp
@@ -1,5 +1,20 @@
 #include "rotateextrudemodule.h"
+#include "node/rotateextrudenode.h"
+#include "numbervalue.h"
 
 RotateExtrudeModule::RotateExtrudeModule() : Module("rotate_extrude")
 {
+}
+
+Node* RotateExtrudeModule::evaluate(Context* ctx)
+{
+	double r=1.0;
+	NumberValue* radius=dynamic_cast<NumberValue*>(ctx->getArgument(0,"radius"));
+	if(radius)
+		r=radius->getNumber();
+
+	RotateExtrudeNode* n=new RotateExtrudeNode();
+	n->setRadius(r);
+	n->setChildren(ctx->inputNodes);
+	return n;
 }
--- src/module/rotateextrudemodule.h
+++ src/module/rotateextrudemodule.h
@@ -2,11 +2,13 @@
 #define ROTATEEXTRUDEMODULE_H
 
 #include "module.h"
+#include "context.h"
 
 class RotateExtrudeModule : public Module
 {
 public:
 	RotateExtrudeModule();
+	Node* evaluate(Context*);
 };
 
 #endif // ROTATEEXTRUDEMODULE_H
