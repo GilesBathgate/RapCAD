8ed01f2f29a70bd54c6274a37e8a215c0ada6e29
Giles Bathgate <gilesbathgate@gmail.com>
2013-12-11 21:09:16 +0000

    Correct path in release-win32.bat

--- release-win32.bat
+++ release-win32.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call c:\Qt\4.8.0\bin\qtvars.bat
+call c:\Qt\Qt5.1.1\5.1.1\mingw48_32\bin\qtenv2.bat
 set PATH=%PATH%;c:\MinGW\msys\1.0\bin
 set PATH=%PATH%;%ProgramFiles%\Git\bin
 set PATH=%PATH%;%ProgramFiles%\NSIS
