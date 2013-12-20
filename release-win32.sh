2c67c2b28a273bafe2e2a928d96b1f58274d713e
Giles Bathgate <gilesbathgate@gmail.com>
2013-12-20 20:38:43 +0000

    Make windows release script exit on fail.

--- release-win32.sh
+++ release-win32.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function die {
+  echo $1
+  exit
+}
+
 hostdir=/h/rapcad
 build=$hostdir/build.log
 error=$hostdir/error.log
@@ -7,47 +12,47 @@ error=$hostdir/error.log
 pushd /c/rapcad
 
 git pull \
-  > $build 2> $error
+  > $build 2> $error || die "failed on git pull"
 
-version=$(cat VERSION)
+version=$(cat VERSION) || exit
 
 echo Building RapCAD v$version \
-  >> $build
+  >> $build || exit
 
 git reset --hard master \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed on git reset"
 
 git clean -df \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed on git clean"
 
 qmake CONFIG+=official \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "qmake failed."
 
 mingw32-make -f Makefile.Release \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "make failed."
 
 mingw32-make clean \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "make clean failed."
 
 cp ../rapcad-dlls/* release \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed copying dlls."
 
 makensis installer.nsi \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed building nsis installer."
 
 mv rapcad_setup.exe rapcad_$version\_setup.exe \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed renaming setup exe"
 
 mv release rapcad-$version  \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed making release dir"
 
 7z a -tzip rapcad_$version.zip rapcad-$version \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed building zip"
 
 mv rapcad_$version\_setup.exe $hostdir \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed moving zip"
 
 mv rapcad_$version.zip $hostdir \
-  >> $build 2>> $error
+  >> $build 2>> $error || die "failed moving setup exe"
 
 popd
