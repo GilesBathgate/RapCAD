7abf1b26942d4e9c4a5449c3ade475775109e37f
Giles Bathgate <gilesbathgate@gmail.com>
2013-12-20 19:42:23 +0000

    Now using Qt 5.2.0 for windows builds.

--- release-win32.bat
+++ release-win32.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call c:\Qt\Qt5.1.1\5.1.1\mingw48_32\bin\qtenv2.bat
+call c:\Qt\Qt5.2.0\5.2.0\mingw48_32\bin\qtenv2.bat
 set PATH=%PATH%;%ProgramFiles%\Git\bin
 set PATH=%PATH%;%ProgramFiles%\NSIS
 set PATH=%PATH%;%ProgramFiles%\7-Zip
