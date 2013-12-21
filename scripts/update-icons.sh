ef2cf21b35eedb23579e18f001acddc045243716
Giles Bathgate <gilesbathgate@gmail.com>
2013-12-21 11:56:52 +0000

    Update icons now prints some messages.

--- scripts/update-icons.sh
+++ scripts/update-icons.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+pushd ../
 echo '<!DOCTYPE RCC><RCC version="1.0">' > src/icons.qrc
 echo '<qresource prefix="icons/gnome">' >> src/icons.qrc
 echo '    <file alias="index.theme">../icons/gnome/index.theme</file>' >> src/icons.qrc
 
 sed -n 's/.*iconset theme="\(.*\)".*/\1.png/p' src/mainwindow.ui | while read img
 do
-cp /usr/share/icons/gnome/22x22/*/$img icons/gnome/22x22/
-cp /usr/share/icons/gnome/16x16/*/$img icons/gnome/16x16/
-echo "    <file alias=\"22x22/$img\">../icons/gnome/22x22/$img</file>" >> src/icons.qrc
-echo "    <file alias=\"16x16/$img\">../icons/gnome/16x16/$img</file>" >> src/icons.qrc
+  cp /usr/share/icons/gnome/22x22/*/$img icons/gnome/22x22/
+  cp /usr/share/icons/gnome/16x16/*/$img icons/gnome/16x16/
+  echo "Updated $img"
+  echo "    <file alias=\"22x22/$img\">../icons/gnome/22x22/$img</file>" >> src/icons.qrc
+  echo "    <file alias=\"16x16/$img\">../icons/gnome/16x16/$img</file>" >> src/icons.qrc
 done
 echo '</qresource>' >> src/icons.qrc
 echo '</RCC>' >> src/icons.qrc
-
+echo "Updated icons.qrc"
+popd
