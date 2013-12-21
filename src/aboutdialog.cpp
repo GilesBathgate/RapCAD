a74bffb7a1e3fd55a10a616e050078def696cfe6
Giles Bathgate <gilesbathgate@gmail.com>
2013-12-21 20:03:03 +0000

    Add Qt version as well and use textbox instead of listview.

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -1,11 +1,10 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 #include "stringify.h"
-
+#include "qglobal.h"
 #if USE_CGAL
 #include <CGAL/version.h>
 #endif
-
 #include <boost/version.hpp>
 #include <dxflib/dl_dxf.h>
 
@@ -23,11 +22,12 @@ AboutDialog::AboutDialog(QWidget* parent) :
 	ui->picture->setPixmap(rapcadPicture);
 
 	ui->name->setText("RapCAD Version " + QSTRINGIFY(RAPCAD_VERSION));
+	ui->components->appendPlainText("Qt\tVersion: " + QString(QT_VERSION_STR));
 #if USE_CGAL
-	ui->components->addItem("CGAL Version: " + QString(CGAL_VERSION_STR));
+	ui->components->appendPlainText("CGAL\tVersion: " + QString(CGAL_VERSION_STR));
 #endif
-	ui->components->addItem("Boost Version: " + QString(BOOST_LIB_VERSION));
-	ui->components->addItem("Dxflib Version: " + QString(DL_VERSION));
+	ui->components->appendPlainText("Boost\tVersion: " + QString(BOOST_LIB_VERSION));
+	ui->components->appendPlainText("Dxflib\tVersion: " + QString(DL_VERSION));
 }
 
 AboutDialog::~AboutDialog()
