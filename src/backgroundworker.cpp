GilesBathgate/RapCAD

commit a5330e4fc5b269518613741414f17985f04bfc99

Refactor worker so that it is no longer a QObject.

src/backgroundworker.cpp
@@ -19,10 +19,11 @@
 #include "backgroundworker.h"
 
 BackgroundWorker::BackgroundWorker(QTextStream& s,QObject* parent) :
-	Worker(s,parent)
+	QObject(parent),
+	Worker(s)
 {
 	thread=new QThread();
-	connect(thread,SIGNAL(started()),this,SLOT(evaluateInternal()));
+	connect(thread,SIGNAL(started()),this,SLOT(start()));
 	this->moveToThread(thread);
 }
 
@@ -36,7 +37,18 @@ void BackgroundWorker::evaluate()
 	thread->start();
 }
 
+void BackgroundWorker::start()
+{
+	internal();
+}
+
+void BackgroundWorker::update()
+{
+	emit done();
+}
+
 void BackgroundWorker::finish()
 {
+	update();
 	thread->quit();
 }

rapcad.pro
@@ -435,7 +435,8 @@ HEADERS  += \
 	src/module/projectionmodule.h \
 	src/node/projectionnode.h \
 	src/tester.h \
-	src/cgalassert.h
+	src/cgalassert.h \
+	src/strategy.h
 
 FORMS += \
 	src/mainwindow.ui \

src/backgroundworker.h
@@ -19,17 +19,24 @@
 #ifndef BACKGROUNDWORKER_H
 #define BACKGROUNDWORKER_H
 
+#include <QObject>
 #include <QThread>
+#include <QTextStream>
 #include "worker.h"
 
-class BackgroundWorker : public Worker
+class BackgroundWorker : public QObject , public Worker
 {
 	Q_OBJECT
 public:
 	BackgroundWorker(QTextStream&,QObject* parent = 0);
 	virtual ~BackgroundWorker();
 	void evaluate();
+signals:
+	void done();
+protected slots:
+	void start();
 private:
+	void update();
 	void finish();
 	QThread* thread;
 };

src/tester.cpp
@@ -21,7 +21,7 @@
 #include "nodeprinter.h"
 #include "booleanvalue.h"
 
-Tester::Tester(QTextStream& s,QObject* parent) : Worker(s,parent)
+Tester::Tester(QTextStream& s) : Worker(s)
 {
 }
 

src/tester.h
@@ -23,9 +23,8 @@
 
 class Tester : public Worker
 {
-	Q_OBJECT
 public:
-	Tester(QTextStream&,QObject* parent = 0);
+	Tester(QTextStream&);
 	void evaluate();
 private:
 	bool testFunctionExists(Script *s);

src/worker.cpp
@@ -31,8 +31,7 @@
 #include "cgalexplorer.h"
 #endif
 
-Worker::Worker(QTextStream& s, QObject* parent) :
-	QObject(parent),
+Worker::Worker(QTextStream& s) :
 	output(s)
 {
 	primitive=NULL;
@@ -59,10 +58,10 @@ void Worker::setup(QString i,QString o,bool p,bool g)
 
 void Worker::evaluate()
 {
-	evaluateInternal();
+	internal();
 }
 
-void Worker::evaluateInternal()
+void Worker::internal()
 {
 
 	try {
@@ -72,24 +71,20 @@ void Worker::evaluateInternal()
 
 		reporter->reportTiming();
 
-		emit done();
+		update();
 
 		if(generate)
 			generation();
 
 #if USE_CGAL
 	} catch(CGAL::Assertion_exception e) {
-		output << "What: " << QString::fromStdString(e.what()) << "\n";
-		output.flush();
-		emit done();
+		output << "What: " << QString::fromStdString(e.what()) << endl;
 	} catch(...) {
-		emit done();
 	}
 #else
 	}
 	catch(...)
 	{
-		emit done();
 	}
 #endif
 
@@ -167,7 +162,7 @@ void Worker::generation()
 			primitive=ne->getResult();
 			delete ne;
 
-			emit done();
+			update();
 		}
 	}
 	delete e;
@@ -224,10 +219,6 @@ Instance* Worker::addProductInstance(QString name,Script* s)
 	return m;
 }
 
-void Worker::finish()
-{
-}
-
 void Worker::exportResult(QString fn)
 {
 #if USE_CGAL
@@ -238,8 +229,7 @@ void Worker::exportResult(QString fn)
 			exporter.exportResult(fn);
 		}
 	} catch(CGAL::Assertion_exception e) {
-		output << "What: " << QString::fromStdString(e.what()) << "\n";
-		output.flush();
+		output << "What: " << QString::fromStdString(e.what()) << endl;
 	}
 #endif
 }

src/worker.h
@@ -21,6 +21,7 @@
 
 #include <QObject>
 #include <QTextStream>
+#include "strategy.h"
 #include "primitive.h"
 #include "renderer.h"
 #include "reporter.h"
@@ -31,23 +32,21 @@
 
 extern Script* parse(QString,Reporter*);
 
-class Worker : public QObject
+class Worker : Strategy
 {
-	Q_OBJECT
 public:
-	Worker(QTextStream&,QObject* parent = 0);
+	Worker(QTextStream&);
 	void setup(QString,QString,bool,bool);
 	virtual void evaluate();
 	void exportResult(QString);
 	bool resultAvailable();
 	Renderer* getRenderer();
 	virtual ~Worker();
-signals:
-	void done();
-protected slots:
-	void evaluateInternal();
+
 protected:
-	virtual void finish();
+	void internal();
+	virtual void update() {}
+	virtual void finish() {}
 	Instance* addProductInstance(QString,Script*);
 	Callback* addCallback(QString,Script*,QList<Argument*>);
 	QTextStream& output;

commit e1e214bba0a84a477e4669a558c1c5233d6ca517

Fix bug where update gets called twice.
Bug introduced in commit a5330e4fc5b269518613741414f17985f04bfc99

src/backgroundworker.cpp
@@ -50,6 +50,5 @@ void BackgroundWorker::update()
 
 void BackgroundWorker::finish()
 {
-	update();
 	thread->quit();
 }