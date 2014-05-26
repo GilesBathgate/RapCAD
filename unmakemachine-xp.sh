38dace5cb1f67867a06ad2dfa00c6a2926f6c92b
Giles Bathgate <gilesbathgate@gmail.com>
2014-05-26 10:52:30 +0100

    Remove SATA device in unmakemachine.

--- unmakemachine-xp.sh
+++ unmakemachine-xp.sh
@@ -5,7 +5,7 @@ VBoxManage storageattach "$NAME" --storagectl "IDE Controller" --port 1 --device
 
 VBoxManage storageattach "$NAME" --storagectl "IDE Controller" --port 0 --device 1 --type dvddrive --medium none
 
-VBoxManage storageattach "$NAME" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium none
+VBoxManage storageattach "$NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium none
 
 VBoxManage storageattach "$NAME" --storagectl "Floppy Controller" --port 0 --device 0 --type fdd --medium none
 
