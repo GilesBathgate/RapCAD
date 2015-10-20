9bcb17da8e35e14281639e5f6d42e9523e9af188
Giles Bathgate <gilesbathgate@gmail.com>
2015-10-20 19:36:21 +0100

    Move some files out of root dir.

--- scripts/astyle.sh
+++ scripts/astyle.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+RC="$(dirname $0)/astylerc"
 files=$(git status -s | grep "^[^ D] .*\(h\|cpp\)$" | cut -d' ' -f3)
 #Apply astyle rules to files that would be commited. 
 if [ -n "$files" ]; then
-  astyle --options=astylerc $files
+  astyle --options=$RC $files
 else
   echo "You need to add some files to the index first!"
 fi
