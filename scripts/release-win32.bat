9c30a46112e24cf230a500308ba2805d6490b9ca
Giles Bathgate <gilesbathgate@gmail.com>
2015-10-24 19:59:31 +0100

    Update to Qt5.5.1 on windows build.

--- scripts/release-win32.bat
+++ scripts/release-win32.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call c:\Qt\Qt5.2.0\5.2.0\mingw48_32\bin\qtenv2.bat
+call c:\Qt\Qt5.5.1\5.5\mingw492_32\bin\qtenv2.bat
 set PATH=%PATH%;%ProgramFiles%\Git\bin
 set PATH=%PATH%;%ProgramFiles%\NSIS
 set PATH=%PATH%;%ProgramFiles%\7-Zip
