b76cf641c9b9e940e3fcdb11ff5a1d10ecf2dc0a
Giles Bathgate <gilesbathgate@gmail.com>
2015-11-06 21:11:17 +0000

    Split start-vm from fullscript.

---
+++ scripts/vbox/start-vm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source config
+
+VBoxHeadless -startvm "$NAME" &
+
+echo "Wait for machine to be ready..."
+echo -n "Waiting"
+until vboxmanage guestcontrol "$NAME" \
+        stat C:\\ \
+        --username $USER \
+        --password $PASS 2> /dev/null
+do
+ echo -n "."
+ sleep 1
+done
+
+echo "Attempting login..."
+vboxmanage controlvm "$NAME" \
+        setcredentials $USER $PASS localhost \
+        --allowlocallogon yes
+
+echo -n "Waiting"
+until vboxmanage guestcontrol "$NAME" \
+        stat D:\\ \
+        --username $USER \
+        --password $PASS
+do
+ echo -n "."
+ sleep 1
+done
+
+echo "Ready to go..."
+echo "$USER password: $PASS"
