0aa200533b44ac235df36fe402ea9e0a0421398c
Giles Bathgate <gilesbathgate@gmail.com>
2017-10-19 19:24:03 +0100

    Fix formatting in tester.cpp

--- src/tester.cpp
+++ src/tester.cpp
@@ -46,69 +46,69 @@ int Tester::evaluate()
 	 * but it will do for now. */
 	QDir cur=QDir::current();
 	foreach(QString dir, cur.entryList(QStringList("*_*")))
-	foreach(QFileInfo file, QDir(dir).entryInfoList(QStringList("*.rcad"),QDir::Files)) {
+		foreach(QFileInfo file, QDir(dir).entryInfoList(QStringList("*.rcad"),QDir::Files)) {
 
-		output << "Test #" << QString().setNum(testcount+1).rightJustified(3,'0') << ": ";
-		output << file.fileName().leftJustified(62,'.',true);
-		output.flush();
+			output << "Test #" << QString().setNum(testcount+1).rightJustified(3,'0') << ": ";
+			output << file.fileName().leftJustified(62,'.',true);
+			output.flush();
 
-		Script* s=parse(file.absoluteFilePath(),NULL,true);
+			Script* s=parse(file.absoluteFilePath(),NULL,true);
 
-		TreeEvaluator te(&nullreport);
+			TreeEvaluator te(&nullreport);
 
-		if(testFunctionExists(s)) {
-			//If a test function exists check it returns true
-			Callback* c = addCallback("test",s,args);
-			s->accept(te);
-			BooleanValue* v = dynamic_cast<BooleanValue*>(c->getResult());
-			if(v && v->isTrue()) {
-				output << " Passed" << endl;
-			} else {
-				output << " FAILED" << endl;
-				failcount++;
-			}
-			delete v;
-
-			Node* n=te.getRootNode();
-			delete n;
-		} else {
-			QString basename=file.baseName();
-			QString examFileName=basename + ".exam.csg";
-			QString csgFileName=basename + ".csg";
-			QFileInfo examFileInfo(file.absoluteDir(),examFileName);
-			QFileInfo csgFileInfo(file.absoluteDir(),csgFileName);
-			QFile examFile(examFileInfo.absoluteFilePath());
-			s->accept(te);
-
-			//Create exam file
-			examFile.open(QFile::WriteOnly);
-			QTextStream examout(&examFile);
-			NodePrinter p(examout);
-			Node* n=te.getRootNode();
-			n->accept(p);
-			delete n;
-			examout.flush();
-			examFile.close();
-
-			QFile csgFile(csgFileInfo.absoluteFilePath());
-			if(csgFile.exists()) {
-				Comparer co(nullout);
-				co.setup(examFileInfo.absoluteFilePath(),csgFileInfo.absoluteFilePath());
-				if(co.evaluate()==0) {
+			if(testFunctionExists(s)) {
+				//If a test function exists check it returns true
+				Callback* c = addCallback("test",s,args);
+				s->accept(te);
+				BooleanValue* v = dynamic_cast<BooleanValue*>(c->getResult());
+				if(v && v->isTrue()) {
 					output << " Passed" << endl;
 				} else {
 					output << " FAILED" << endl;
 					failcount++;
 				}
-				examFile.remove();
-			} else {
-				output << "Created" << endl;
-			}
+				delete v;
 
+				Node* n=te.getRootNode();
+				delete n;
+			} else {
+				QString basename=file.baseName();
+				QString examFileName=basename + ".exam.csg";
+				QString csgFileName=basename + ".csg";
+				QFileInfo examFileInfo(file.absoluteDir(),examFileName);
+				QFileInfo csgFileInfo(file.absoluteDir(),csgFileName);
+				QFile examFile(examFileInfo.absoluteFilePath());
+				s->accept(te);
+
+				//Create exam file
+				examFile.open(QFile::WriteOnly);
+				QTextStream examout(&examFile);
+				NodePrinter p(examout);
+				Node* n=te.getRootNode();
+				n->accept(p);
+				delete n;
+				examout.flush();
+				examFile.close();
+
+				QFile csgFile(csgFileInfo.absoluteFilePath());
+				if(csgFile.exists()) {
+					Comparer co(nullout);
+					co.setup(examFileInfo.absoluteFilePath(),csgFileInfo.absoluteFilePath());
+					if(co.evaluate()==0) {
+						output << " Passed" << endl;
+					} else {
+						output << " FAILED" << endl;
+						failcount++;
+					}
+					examFile.remove();
+				} else {
+					output << "Created" << endl;
+				}
+
+			}
+			delete s;
+			testcount++;
 		}
-		delete s;
-		testcount++;
-	}
 
 	reporter->setReturnCode(failcount);
 
