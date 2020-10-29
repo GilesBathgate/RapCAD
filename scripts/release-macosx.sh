69c0afce1f31773963f42b526da819d014645078
Giles Bathgate <gilesbathgate@gmail.com>
2020-10-29 08:01:23 +0000

    Remove legacy build scripts

--- scripts/release-macosx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-qmake CONFIG+=official
-make
-cp -R /opt/local/lib/Resources/qt_menu.nib rapcad.app/Contents/Resources/
-macdeployqt rapcad.app -dmg
-hdiutil internet-enable -yes rapcad.dmg
--- scripts/release.sh
+++ scripts/release.sh
@@ -34,14 +34,4 @@ ppa_build "Bionic"
 
 rm -rf rapcad-$version
 
-echo Building Windows version
-if [ ! -d "$windir" ]; then
- mkdir -p $windir
-fi
-popd
-scripts/vbox/start-vm.sh &&
-scripts/vbox/cmd.sh c:\\rapcad\\scripts\\release-win32.bat &&
-mv scripts/vbox/shared/rapcad_$version\_setup.exe ../$windir/ &&
-mv scripts/vbox/shared/rapcad_$version.zip ../$windir/
-
 echo "Complete"
